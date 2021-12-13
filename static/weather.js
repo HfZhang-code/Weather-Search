ac7cc9908a58d48c746bdd43be7e1395d324756a
Nalaaaaa <mzhf201643@gmail.com>
2021-12-13 14:39:51 -0600

    final commit

--- static/weather.js
+++ static/weather.js
@@ -1,9 +1,10 @@
 const testUrl = "http://api.openweathermap.org/data/2.5/weather?q=Boston&appid=5fc3c02339d5c6799f67a62e8c412b51&units=metric&lang=zh_cn;"
 const baseURL = "http://api.openweathermap.org/data/2.5/weather?appid=5fc3c02339d5c6799f67a62e8c412b51&units=metric"
-
+var fah
 $(function () {
     initPage();
 })
+
 
 class APIURL{
     constructor(city){
@@ -18,30 +19,13 @@ var initPage = () => {
     let city = "Boston";
     var geturl = new APIURL(city)
     let url =geturl.getURL();
-    // let url = baseURL + "&q=" + city;
+
     fetch(url)
     .then(response => response.json())
     .then(data => initWeather(data))
     .catch(err => {
         console.error(err);
-
-    });
-
-    // $.ajax({
-    //     url: url,
-    //     method: "get",
-    //     dataType: 'jsonp',
-    //     contentType: 'application/json',
-    //     timeout: 2000,
-    //     success: function (res) {
-    //         initWeather(res);
-    //     },
-    //     // jsonpCallback: "initWeather",
-    //     error: (jqXHR, textStatus) => {
-    //         // console.log(textStatus)
-    //     }
-    // })
-
+    })
 
 }
 
@@ -55,7 +39,6 @@ function doSearch() {
     }
 
     let url = baseURL + "&q=" + city;
-
     fetch(url)
     .then(response => response.json())
     .then(data => initWeather(data))
@@ -64,21 +47,41 @@ function doSearch() {
 
     });
 
-    console.log("search completely!!!!!!!");
 
+}
+// when user type something in the input,location will also change
+function changeLoc(){
+    var location = document.getElementById("searchCity").value;
+    document.getElementById("showCity").innerHTML=location;
+}
+//when user focus on the input element, the background of the input will change
+function changeBack(a){
+    a.style.background="cyan";
+}
 
+//if page can't load the data, it will gey alert message
+function errAlert(){
+    alert("Some errors happened. Please reload the page.")
+}
+
+//when user click on the Date element, the color of content will change
+function turnYellow(a){
+    a.style.color="yellow"
+}
+function turnWhite(a){
+    a.style.color="white"
+}
+
+//when the value of select element change,the color will change.
+function changeColor(){
+    var x = document.getElementById("backcolor").value;
+    document.getElementById("showCity").style.color=x;
+    document.getElementById("temp").style.color=x;
 
 }
 
 var initWeather = (res) => {
-    // console.log(res);
 
-    // document.getElementById("showCity").innerText = res.name;
-    // document.getElementById("desc").innerText = res.weather[0].description;
-    // document.getElementById("wDate").innerText = now;
-    // document.getElementById("temp").innerText = res.main.temp + "°";
-    // document.getElementById("desc1").innerText = res.weather[0].main;
-    // document.getElementById("wind").innerText = " | Wind Speed " + res.wind.speed
 
     $("#showCity").text(res.name);
     $("#desc").text(res.weather[0].description);
@@ -87,16 +90,28 @@ var initWeather = (res) => {
 
 
     let now = new Date();
-    $("#wDate").text(now);
-    $("#temp").text(res.main.temp + "°");
+    let nowStr = "";
+    var y = now.getFullYear();
+    var m = now.getMonth() + 1;
+    var d = now.getDate();
+    var h = now.getHours();
+    var mm = now.getMinutes();
+    var ss = now.getSeconds();
+    nowStr = y + "-" + m + "-" + d + " " + h + ":" + mm + ":" + ss;
+    $("#wDate").text(nowStr);
+    $("#temp").text(res.main.temp + "℃");
+    fah=(res.main.temp * 1.8) + 32
     $("#desc1").text(res.weather[0].main);
-    $("#wind").text(" | Wind Speed " + res.wind.speed);
+    $("#wind").text("Wind Speed " + res.wind.speed);
 
     $(".feels_like").text("Feels Like: " + res.main.feels_like);
-    $(".temp_min").text(" | Temp Min: " + res.main.temp_min);
-    $(".temp_max").text(" | Temp Max: " + res.main.temp_max);
-    $(".pressure").text(" | Pressure: " + res.main.pressure);
-    $(".humidity").text(" | Humidity: " + res.main.humidity);
-    console.log(res);
+    $(".temp_min").text("Temp Min: " + res.main.temp_min);
+    $(".temp_max").text("Temp Max: " + res.main.temp_max);
+    $(".pressure").text("Pressure: " + res.main.pressure);
+    $(".humidity").text("Humidity: " + res.main.humidity);
 
+}
+//change Celsius degree to Hahrenheit drgree
+function toFahren(){
+    $('#temp').text(fah + "℉")
 }
